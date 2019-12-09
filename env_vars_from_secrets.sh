d23f41ca986806064a31ac12d90d4d850cdfc0da
davidsuart <davidsuart@users.noreply.github.com>
2019-12-09 15:08:30 +0000

    add nginx/php interface and supervisord to run it

--- env_vars_from_secrets.sh
+++ env_vars_from_secrets.sh
@@ -3,28 +3,37 @@
 echo "---------- Testing secrets manager json input ----------"
 echo ""
 
-# fetch the contents of our ENV environment variable
+# fetch the contents of our ENV input variable
+#
 echo "Show the received input value in \$ENV:"
 echo " \$ENV = $ENV"
 echo ""
 
+# ensure our vars do not have anything in them
+#
 echo "Show that our target env vars are empty:"
 echo "  \$ENV_TEST_ONE     = $ENV_TEST_ONE"
 echo "  \$ENV_TEST_TWO     = $ENV_TEST_TWO"
 echo "  \$ENV_TEST_EXTRA   = $ENV_TEST_EXTRA"
 echo ""
 
-# split out the keys and values and export them
-echo "Create the environment variables:"
-$( echo "$ENV" | jq -r 'keys[] as $k | "export \($k)=\(.[$k])"' )
+# use jq to enumerate the keys and values and pass to export
 #
+echo "Split and export the keys and values from \$ENV ..."
+echo "  \$( echo \"\$ENV\" | jq -r 'keys[] as \$k | \"export \(\$k)=\(.[\$k])\"' )"
+$( echo "$ENV" | jq -r 'keys[] as $k | "export \($k)=\(.[$k])"' )
+echo ""
+
+# check our vars are now populated
+#
+echo "And verify them:"
 echo "  \$ENV_TEST_ONE   = $ENV_TEST_ONE"
 echo "  \$ENV_TEST_TWO   = $ENV_TEST_TWO"
 echo "  \$ENV_TEST_EXTRA = $ENV_TEST_EXTRA"
+echo ""
 
-# cleanup the vars
+# cleanup
+#
 unset ENV_TEST_ONE
 unset ENV_TEST_TWO
 unset ENV_TEST_EXTRA
-
-echo ""
--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+# Artifical delay for demo (To keep console output cosmetically clean)
+sleep 5
+
 /bin/sh "env_vars_from_secrets.sh"
 
 echo "---------- Execution completed ----------"
